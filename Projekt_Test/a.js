610c975120669f1b751e07e05a5342faa3d3d656
UsernameTakenOof <weibert.alexander@web.de>
2024-01-24 23:50:33 +0100

    Update a.js

--- Projekt_Test/a.js
+++ Projekt_Test/a.js
@@ -576,9 +576,9 @@ const blockVAO = glance.createVAO(
 )
 
 
-const blockTextureAmbient = await glance.loadTextureNow(gl, "Tetrix/img/block_ambient.png")
-const blockTextureDiffuse = await glance.loadTextureNow(gl, "Tetrix/img/block_diffuse.png")
-const blockTextureSpecular = await glance.loadTextureNow(gl, "Tetriximg/block_specular.png")
+const blockTextureAmbient = await glance.loadTextureNow(gl, ".Tetrix/img/block_ambient.png")
+const blockTextureDiffuse = await glance.loadTextureNow(gl, ".Tetrix/img/block_diffuse.png")
+const blockTextureSpecular = await glance.loadTextureNow(gl, ".Tetriximg/block_specular.png")
 
 
 
