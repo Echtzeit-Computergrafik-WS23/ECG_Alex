5aad0f6f00f838335097629df481667c81529a23
weibertalexander <91937816+weibertalexander@users.noreply.github.com>
2024-01-25 08:35:45 +0100

    Update a.js

--- Tetrix/a.js
+++ Tetrix/a.js
@@ -576,7 +576,7 @@ const blockVAO = glance.createVAO(
 )
 
 
-const blockTextureAmbient = await glance.loadTextureNow(gl, "/img/block_ambient.png")
+const blockTextureAmbient = await glance.loadTextureNow(gl, "./img/block_ambient.png")
 const blockTextureDiffuse = await glance.loadTextureNow(gl, "./img/block_diffuse.png")
 const blockTextureSpecular = await glance.loadTextureNow(gl, "./img/block_specular.png")
 
